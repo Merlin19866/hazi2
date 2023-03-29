38f927aa0ad2412e05ade9f6721b0baa57e72181
Kiss Tamas <tamas@1986>
2023-03-29 22:03:51 +0200

    masodik

--- src/main/java/com/merlin/hazi2/Hazi2.java
+++ src/main/java/com/merlin/hazi2/Hazi2.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.merlin.hazi2;
 
-/**
- *
- * @author Gazda
- */
 public class Hazi2 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        
     }
 }
