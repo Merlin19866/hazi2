a5077b02834d45039d6eb58eb249861a0ab9d6c0
Kiss Tamas <tamas@1986>
2023-03-30 21:59:18 +0200

    harmadik

--- src/main/java/com/merlin/hazi2/WishItem.java
+++ src/main/java/com/merlin/hazi2/WishItem.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.merlin.hazi2;
 
-/**
- *
- * @author Gazda
- */
-public class WishItem {
-    
+public class WishItem
+{
+    private String name;
+    private int price;
+    public int savedmoney;
 }
