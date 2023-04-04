f0d5c2077fd1205f3ecf2933807d76576f3a77e0
Kiss Tamas <tamas@1986>
2023-04-04 05:48:04 +0200

    negyedik

--- src/main/java/com/merlin/hazi2/WishItem.java
+++ src/main/java/com/merlin/hazi2/WishItem.java
@@ -5,4 +5,27 @@ public class WishItem
     private String name;
     private int price;
     public int savedmoney;
+
+    public WishItem(String name, int price, int savedmoney) {
+        this.name = name;
+        this.price = price;
+        this.savedmoney = savedmoney;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public void setPrice(int price) 
+    {
+        this.price = price;
+    }
+
+    public void setSavedmoney(int savedmoney) 
+    {
+        this.savedmoney = savedmoney;
+    }
+    
+    
 }
